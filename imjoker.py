1324b4ee5eeff162a3dd914da020db4407c86019
saikiran4888 <43513392+saikiran4888@users.noreply.github.com>
2019-05-06 20:30:20 +0530

    Update imjoker.py

--- imjoker.py
+++ imjoker.py
@@ -17,7 +17,7 @@ import aiohttp
 
 async def status_task():
     while True:
-        await client.change_presence(game=discord.Game(name='REFORMED COMMUNITY', type=1))
+        await client.change_presence(game=discord.Game(name='๖ۣۜζ͜͡VIRTUAL REFORMED', type=1))
         
       
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
