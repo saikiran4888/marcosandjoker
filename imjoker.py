8178acbeafe94ca49bd7d5576cc31f21125923b8
saikiran4888 <43513392+saikiran4888@users.noreply.github.com>
2019-05-22 10:42:06 +0530

    Update imjoker.py

--- imjoker.py
+++ imjoker.py
@@ -600,6 +600,11 @@ async def unlock(ctx, Role:discord.Role=None, Channel:discord.Channel=None):
     overwrite.read_message_history = True
     await client.edit_channel_permissions(Channel, Role, overwrite = overwrite)
     
+    
+@client.command(pass_context = True)
+async def hlo(ctx):
+    await client.send_message(ctx.message.channel, f"{ctx.message.author.mention}, How you're doing?")
+    
 
   
 client.run(os.getenv('Token'))
